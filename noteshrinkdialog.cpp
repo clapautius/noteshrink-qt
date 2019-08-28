afc2557e0637b376dbd207925066931831cef43c
Tudor M. Pristavu <clapautius@users.noreply.github.com>
2019-08-28 20:45:34 +0300

    Implement basic preview.

--- noteshrinkdialog.cpp
+++ noteshrinkdialog.cpp
@@ -1,6 +1,9 @@
 #include "noteshrinkdialog.h"
 #include "ui_noteshrinkdialog.h"
 
+#include <QMessageBox>
+#include <QFileDialog>
+
 NoteshrinkDialog::NoteshrinkDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::NoteshrinkDialog)
@@ -11,4 +14,19 @@ NoteshrinkDialog::NoteshrinkDialog(QWidget *parent) :
 NoteshrinkDialog::~NoteshrinkDialog()
 {
     delete ui;
+}
+
+
+void NoteshrinkDialog::on_m_preview_button_box_clicked(QAbstractButton *button)
+{
+    if((QPushButton*)button == ui->m_preview_button_box->button(QDialogButtonBox::Open) ){
+       //QMessageBox::information(nullptr, "Opening preview image", "Opening preview image");
+       m_preview_image_path = QFileDialog::getOpenFileName(nullptr, "Select image");
+       if (!m_preview_image_path.isNull()) {
+           m_preview_image.load(m_preview_image_path);
+           ui->m_preview_area->setPixmap(QPixmap::fromImage(m_preview_image));
+           ui->m_preview_area->setScaledContents(true);
+           ui->m_preview_area->setSizePolicy( QSizePolicy::Ignored, QSizePolicy::Ignored );
+       }
+    }
 }
--- noteshrinkdialog.h
+++ noteshrinkdialog.h
@@ -2,6 +2,9 @@
 #define NOTESHRINKDIALOG_H
 
 #include <QDialog>
+#include <QAbstractButton>
+#include <QFile>
+#include <QImage>
 
 namespace Ui {
 class NoteshrinkDialog;
@@ -12,11 +15,20 @@ class NoteshrinkDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit NoteshrinkDialog(QWidget *parent = 0);
+    explicit NoteshrinkDialog(QWidget *parent = nullptr);
     ~NoteshrinkDialog();
+
+private slots:
+    void on_m_preview_button_box_clicked(QAbstractButton *button);
 
 private:
     Ui::NoteshrinkDialog *ui;
+
+    QString m_preview_image_path;
+
+    QFile m_preview_image_file_tmp;
+
+    QImage m_preview_image;
 };
 
 #endif // NOTESHRINKDIALOG_H
