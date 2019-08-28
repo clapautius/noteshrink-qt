f53cc2fb22c2f7d1633120b20856f88ed942f7d6
Tudor M. Pristavu <clapautius@users.noreply.github.com>
2019-08-28 23:13:48 +0300

    Execute noteshrink command. Show preview.

--- noteshrinkdialog.cpp
+++ noteshrinkdialog.cpp
@@ -3,12 +3,17 @@
 
 #include <QMessageBox>
 #include <QFileDialog>
+#include <QProcess>
 
 NoteshrinkDialog::NoteshrinkDialog(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::NoteshrinkDialog)
+    ui(new Ui::NoteshrinkDialog),
+    m_preview_image_tmp_path("/tmp/noteshrink-qt-tmp0000.png") // :fixme: temporary path
 {
     ui->setupUi(this);
+    if (QFile::exists(m_preview_image_tmp_path)) {
+        QFile::remove(m_preview_image_tmp_path);
+    }
 }
 
 NoteshrinkDialog::~NoteshrinkDialog()
@@ -17,16 +22,78 @@ NoteshrinkDialog::~NoteshrinkDialog()
 }
 
 
+void NoteshrinkDialog::update_preview_image()
+{
+    m_preview_image.load(m_preview_image_tmp_path);
+    ui->m_preview_area->setPixmap(QPixmap::fromImage(m_preview_image));
+    ui->m_preview_area->setScaledContents(true);
+    ui->m_preview_area->setSizePolicy( QSizePolicy::Ignored, QSizePolicy::Ignored );
+}
+
+
 void NoteshrinkDialog::on_m_preview_button_box_clicked(QAbstractButton *button)
 {
     if((QPushButton*)button == ui->m_preview_button_box->button(QDialogButtonBox::Open) ){
        //QMessageBox::information(nullptr, "Opening preview image", "Opening preview image");
-       m_preview_image_path = QFileDialog::getOpenFileName(nullptr, "Select image");
-       if (!m_preview_image_path.isNull()) {
-           m_preview_image.load(m_preview_image_path);
-           ui->m_preview_area->setPixmap(QPixmap::fromImage(m_preview_image));
-           ui->m_preview_area->setScaledContents(true);
-           ui->m_preview_area->setSizePolicy( QSizePolicy::Ignored, QSizePolicy::Ignored );
+       m_preview_image_src_path = QFileDialog::getOpenFileName(nullptr, "Select image");
+       if (!m_preview_image_src_path.isNull()) {
+           // copy src to tmp for initial preview
+           if (QFile::copy(m_preview_image_src_path, m_preview_image_tmp_path)) {
+               update_preview_image();
+           } else {
+               QMessageBox::critical(nullptr, "Error", "Cannot create temporary file");
+           }
        }
+    }
+}
+
+
+bool NoteshrinkDialog::run_noteshrink_cmd()
+{
+    bool rc = false;
+    QString cmd = "noteshrink.py ";
+
+    cmd += "-v ";
+    int i = ui->m_bkg_value_thres->value();
+    cmd += QString::number(i);
+
+    // output
+    cmd += " -b ";
+    cmd += m_preview_image_tmp_path.left(m_preview_image_tmp_path.size() - 8);
+
+    cmd += " -p ";
+    cmd += QString::number(ui->m_pixels_sample->value());
+
+    if (ui->m_bkg_white->isChecked()) {
+        cmd += " -w ";
+    }
+
+    cmd += " -c \"/bin/true\" ";
+
+    cmd += m_preview_image_src_path;
+
+    //QMessageBox::information(nullptr, "Command", cmd);
+    ui->m_log_window->appendPlainText("Running command:");
+    ui->m_log_window->appendPlainText(cmd);
+    ui->m_log_window->appendPlainText("");
+
+    if (QProcess::execute(cmd) == 0) {
+        update_preview_image();
+        rc = true;
+    } else {
+        QMessageBox::critical(nullptr, "Error", "Error executing noteshrink");
+    }
+    return rc;
+}
+
+
+void NoteshrinkDialog::on_m_params_button_box_clicked(QAbstractButton *button)
+{
+    if((QPushButton*)button == ui->m_params_button_box->button(QDialogButtonBox::Apply)) {
+        if (run_noteshrink_cmd()) {
+            update_preview_image();
+        } else {
+            QMessageBox::critical(nullptr, "Error", "noteshrink.py error");
+        }
     }
 }
--- noteshrinkdialog.h
+++ noteshrinkdialog.h
@@ -21,12 +21,19 @@ public:
 private slots:
     void on_m_preview_button_box_clicked(QAbstractButton *button);
 
+    void on_m_params_button_box_clicked(QAbstractButton *button);
+
 private:
+
+    void update_preview_image();
+
+    bool run_noteshrink_cmd();
+
     Ui::NoteshrinkDialog *ui;
 
-    QString m_preview_image_path;
+    QString m_preview_image_src_path;
 
-    QFile m_preview_image_file_tmp;
+    QString m_preview_image_tmp_path;
 
     QImage m_preview_image;
 };
