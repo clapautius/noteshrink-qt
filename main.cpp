533d384d02f1cb72c42ca301899d963a739e3574
Tudor M. Pristavu <clapautius@users.noreply.github.com>
2019-09-01 15:16:22 +0300

    Set window icon.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon("noteshrink-qt-icon.png")); // :fixme: add this as resource
     NoteshrinkDialog w;
     w.show();
 
