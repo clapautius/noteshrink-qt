a1f192123747f9c17aead8740d2723a6ed0e1760
Tudor M. Pristavu <clapautius@users.noreply.github.com>
2019-09-02 17:42:53 +0300

    Add new screenshot file. Change version.

--- noteshrinkdialog.h
+++ noteshrinkdialog.h
@@ -9,7 +9,7 @@
 #include <QImage>
 #include <QStringListModel>
 
-#define NOTESHRINK_QT_VER "v2" // :release
+#define NOTESHRINK_QT_VER "v3-beta" // :release
 
 namespace Ui {
 class NoteshrinkDialog;
